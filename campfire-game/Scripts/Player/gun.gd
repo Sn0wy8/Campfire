979ba9e068441b37c3c218656b297cce680e75c7
Snowy <bradleyjaus@gmail.com>
2026-02-28 15:08:14 +1000

    gun yayayayayayay

--- campfire-game/Scripts/Player/gun.gd
+++ campfire-game/Scripts/Player/gun.gd
@@ -12,25 +12,20 @@ var time_between_shot: float = 0.25
 var can_shoot: bool = true
 
 func _ready() -> void:
-	$ShootTimer.wait_time = time_between_shot
-	
+    $ShootTimer.wait_time = time_between_shot
+    
 func _physics_process(delta: float) -> void:
-	RotationOffset.rotation = lerp_angle(RotationOffset.rotation, ( get_global_mouse_position() - global_position ).angle(), 6.5*delta)
-	SpriteShadow.position = Vector2(-2, 2).rotated(-RotationOffset.rotation)
-	
-	if Input.is_action_just_pressed("shoot") and can_shoot:
-		_shoot()
-		can_shoot = false
-		$ShootTimer.start()
-		print("here6")
-	
+    if Input.is_action_just_pressed("shoot") and can_shoot:
+        _shoot()
+        can_shoot = false
+        $ShootTimer.start()
+        print("here6")
+    
 func _shoot():
-	var new_bullet = bullet_scene.instantiate()
-	new_bullet.global_position = ShootPos.global_position
-	new_bullet.global_rotation = ShootPos.global_rotation
-	get_tree().current_scene.add_child(new_bullet)
-	print("ShootPos:", ShootPos.global_position)
-	print("here4")
+    var new_bullet = bullet_scene.instantiate()
+    new_bullet.global_position = ShootPos.global_position
+    new_bullet.global_rotation = ShootPos.global_rotation
+    get_tree().current_scene.add_child(new_bullet)
 
 func _on_shoot_timer_timeout() -> void:
-	can_shoot = true
+    can_shoot = true
