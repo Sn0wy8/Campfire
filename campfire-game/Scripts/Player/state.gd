47ebb903714b4443a7005f134fabd8f8e81ded7a
Snowy <bradleyjaus@gmail.com>
2026-02-28 15:34:42 +1000

    restart

---
+++ campfire-game/Scripts/Player/state.gd
@@ -0,0 +1,23 @@
+class_name State
+extends Node
+
+@export
+var move_speed: float = 300
+
+# Hold a reference to the parent so that it can be controlled by the state
+var parent: Player
+
+func enter() -> void:
+	pass
+
+func exit() -> void:
+	pass
+
+func process_input(event: InputEvent) -> State:
+	return null
+
+func process_frame(delta: float) -> State:
+	return null
+
+func process_physics(delta: float) -> State:
+	return null
