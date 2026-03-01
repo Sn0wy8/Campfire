378528ae5f3fa7c61e9ae3120451e9dcecdc76b0
JammySandwiches <121534073+ILoveBobJaneTMart@users.noreply.github.com>
2026-03-01 14:30:07 +1000

    animations added
    yay i did somethihng

--- campfire-game/Scripts/Player/walk.gd
+++ campfire-game/Scripts/Player/walk.gd
@@ -1,7 +1,7 @@
 extends PlayerState
 @export var idle_state: PlayerState
 @export var dash_state: PlayerState
-
+@onready var animated_sprite: AnimatedSprite2D = $"../../animations" 
 func enter() -> void:
 	pass
 
@@ -12,10 +12,24 @@ func process_input(event: InputEvent) -> PlayerState:
 
 func process_physics(delta: float) -> PlayerState:
 	var movement := Input.get_vector("move_left", "move_right","up","down")
+	
 	parent.velocity = movement * move_speed
 	# Move the character
 	parent.move_and_slide()
-
+	if movement == Vector2.UP:
+		animated_sprite.play("run back")
+		print("1")
+	if movement == Vector2.DOWN:
+		animated_sprite.play("run front")
+		print("2")
+	if movement == Vector2.LEFT:
+		animated_sprite.play("run side")
+		animated_sprite.flip_h = false
+		print("3")
+	if movement == Vector2.RIGHT:
+		animated_sprite.play("run side")
+		animated_sprite.flip_h = true
+		print("4")
 	# Transition to idle if not moving
 	if parent.velocity == Vector2.ZERO:
 		return idle_state
